b5d3205962545326939c93fd1364b1fc60e9a6f7
Ssembatya_Isaac <ssembatyaisaac1999@gmail.com>
2023-02-08 14:14:04 +0000

    Working on icon color

--- lib/main.dart
+++ lib/main.dart
@@ -29,16 +29,66 @@ class AppBarApp extends StatelessWidget {
     final Color oddItemColor = colorScheme.primary.withOpacity(0.05);
     final Color evenItemColor = colorScheme.primary.withOpacity(0.15);
     const int tabsCount = 3;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Whatsapp"),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.add_alert),
-            tooltip: 'Show Snackbar',
-            onPressed: () {},
+    return DefaultTabController(
+      initialIndex: 1,
+      length: tabsCount,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('AppBar Sample'),
+          notificationPredicate: (ScrollNotification notification) {
+            return notification.depth == 1;
+          },
+          scrolledUnderElevation: 4.0,
+          shadowColor: Theme.of(context).shadowColor,
+          bottom: TabBar(
+            labelColor: evenItemColor,
+            tabs: <Widget>[
+              Tab(
+                icon: const Icon(
+                  Icons.cloud_outlined,
+                ),
+                text: titles[0],
+              ),
+              Tab(
+                icon: const Icon(Icons.beach_access_sharp),
+                text: titles[1],
+              ),
+              Tab(
+                icon: const Icon(Icons.brightness_5_sharp),
+                text: titles[2],
+              ),
+            ],
           ),
-        ],
+        ),
+        body: TabBarView(children: <Widget>[
+          ListView.builder(
+            itemCount: 25,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                tileColor: index.isOdd ? oddItemColor : evenItemColor,
+                title: Text('${titles[0]} $index'),
+              );
+            },
+          ),
+          ListView.builder(
+            itemCount: 25,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                tileColor: index.isOdd ? oddItemColor : evenItemColor,
+                title: Text('${titles[1]} $index'),
+              );
+            },
+          ),
+          ListView.builder(
+            itemCount: 25,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                tileColor: index.isOdd ? oddItemColor : evenItemColor,
+                title: Text('${titles[2]} $index'),
+              );
+            },
+          ),
+        ]),
       ),
     );
   }
